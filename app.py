ffe14dd36be25037e2acfb44142b1953210c31ac
Fernando Pimenta <fernando.s.pimenta18@gmail.com>
2024-01-10 11:27:16 -0300

    Implement Vogel WIP (#10)
    * Implement Vogel WIP
    
    * outline of the vogel method WIP
    
    * Update vogel.py
    
    ---------
    
    Co-authored-by: Igor Rosa <igaopinto@gmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from input import create_matrix, columns_headers, rows_headers
-from simplex import Simplex
+import numpy as np
+
+from methods.vogel import Vogel
+from problems.product_distribution import create_matrix
+
+# from problems.input import create_matrix, columns_headers, rows_headers
+# from methods.simplex import Simplex
 
 if __name__ == '__main__':
-    matrix = create_matrix()
-    simplex = Simplex(matrix=matrix, columns_headers=columns_headers, rows_headers=rows_headers)
-    simplex.simplex()
+    # matrix = create_matrix()
+    # simplex = Simplex(matrix=matrix, columns_headers=columns_headers, rows_headers=rows_headers)
+    # simplex.simplex()
+    matrix = [
+        [2, 3, 11, 7, 6],
+        [1, 0, 6, 1, 1],
+        [5, 8, 15, 9, 10],
+        [7, 5, 3, 2, 0]
+    ]
+
+    vogel = Vogel(create_matrix().astype(float))
+    # vogel = Vogel(np.array(matrix).astype(float))
+    vogel.vogel_method()
